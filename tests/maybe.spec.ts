5fd2a8f18c5fb0f591a9445722951e19cbeaacca
ysk8.parchase@gmail.com <ysk8.parchase@gmail.com>
2020-06-10 19:43:46 +0900

    リファクタリング

--- tests/maybe.spec.ts
+++ tests/maybe.spec.ts
@@ -9,29 +9,47 @@ describe('maybe', () => {
   const isOne = (num: number) => num === 1;
   const isTwo = (num: number) => num === 2;
   describe('nullish', () => {
-    test('toEqual', () =>
-      expect(nullish().toString()).toEqual('Maybe.Nothing'));
-    test('map', () =>
-      expect(nullish().map(countUp).toString()).toEqual('Maybe.Nothing'));
-    test('getOrElse', () => expect(nullish().getOrElse(10000)).toBe(10000));
-    test('chain', () =>
-      expect(nullish().chain(toIo).toString()).toEqual('Maybe.Nothing'));
+    test('toEqual', () => {
+      expect(nullish().toString()).toEqual('Maybe.Nothing');
+    });
+    test('map', () => {
+      expect(nullish().map(countUp).toString()).toEqual('Maybe.Nothing');
+    });
+    test('getOrElse', () => {
+      expect(nullish().getOrElse(10000)).toBe(10000);
+    });
+    test('chain', () => {
+      expect(nullish().chain(toIo).toString()).toEqual('Maybe.Nothing');
+    });
     test('filter(withoutNull)', () => {
       expect(nullish().filter(withoutNull).toString()).toEqual('Maybe.Nothing');
     });
-    test('getOrElse', () => expect(nullish().isJust()).toBeFalsy());
-    test('getOrElse', () => expect(nullish().isNothing()).toBeTruthy());
+    test('filter(isOne)', () => {
+      expect(nullish().filter(isOne).toString()).toEqual('Maybe.Nothing');
+    });
+    test('filter(isTwo)', () => {
+      expect(nullish().filter(isTwo).toString()).toEqual('Maybe.Nothing');
+    });
+    test('isJust', () => {
+      expect(nullish().isJust()).toBeFalsy();
+    });
+    test('isNothing', () => {
+      expect(nullish().isNothing()).toBeTruthy();
+    });
   });
   describe('numberish', () => {
-    test('toEqual', () =>
-      expect(numberish().toString()).toEqual('Maybe.Just(1)'));
-    test('map', () =>
-      expect(numberish().map(countUp).toString()).toEqual('Maybe.Just(2)'));
-    test('getOrElse', () => expect(numberish().getOrElse(10000)).toBe(1));
-    test('chain', () =>
-      expect(numberish().chain(toIo).toString()).toEqual(
-        'IO([object Object])'
-      ));
+    test('toEqual', () => {
+      expect(numberish().toString()).toEqual('Maybe.Just(1)');
+    });
+    test('map', () => {
+      expect(numberish().map(countUp).toString()).toEqual('Maybe.Just(2)');
+    });
+    test('getOrElse', () => {
+      expect(numberish().getOrElse(10000)).toBe(1);
+    });
+    test('chain', () => {
+      expect(numberish().chain(toIo).toString()).toEqual('IO([object Object])');
+    });
     test('filter(withoutNull)', () => {
       expect(numberish().filter(withoutNull).toString()).toEqual(
         'Maybe.Just(1)'
@@ -43,7 +61,11 @@ describe('maybe', () => {
     test('filter(isTwo)', () => {
       expect(numberish().filter(isTwo).toString()).toEqual('Maybe.Nothing');
     });
-    test('getOrElse', () => expect(numberish().isJust()).toBeTruthy());
-    test('getOrElse', () => expect(numberish().isNothing()).toBeFalsy());
+    test('isJust', () => {
+      expect(numberish().isJust()).toBeTruthy();
+    });
+    test('isNothing', () => {
+      expect(numberish().isNothing()).toBeFalsy();
+    });
   });
 });
